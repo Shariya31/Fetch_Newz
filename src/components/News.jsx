9c0d967665c0d603fab04651dc1228778296fbee
Shariya31 <abushariya31@gmail.com>
2024-02-23 19:48:56 -0800

    fixed css and api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,31 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import News_items from './News_items'
 import axios from 'axios'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { v4 as uuidv4 } from 'uuid';
+
 const News = (props) => {
     const [news, setNews] = useState([]);
     const [category, setCategory] = useState(props.category)
+    const [page, setPage] = useState(1)
+    const [totalResults, setTotalResults] = useState(0)
 
     const getNews = async () => {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=9bc603a4643a4f3aa5844343c35c5558&category=${category}`);
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=9bc603a4643a4f3aa5844343c35c5558&category=${category}&pageSize=3`);
+        setNews(response.data.articles);
+        setTotalResults(response.data.totalResults);
+    }
 
-        setNews(response.data.articles)
+
+    const fetchMoreData = async ()=>{
+        setPage(page + 1)
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=9bc603a4643a4f3aa5844343c35c5558&category=${category}&page=${page}&pageSize=3`);
+        setNews(news.concat(response.data.articles))
+        setTotalResults(response.data.totalResults)
     }
 
     useEffect(() => {
         getNews()
-        
+
     }, [])
-    console.log(news)
+  
     return (
-        <div className='news_container'>
-            <div className="row">
-                {news.map((element) => (
-                    <div className="col-md-4" key={element.url}>
-                        <News_items author={element.author} content={element.content} description={element.description} publishedAt={element.publishedAt} title={element.title} url={element.url} urlToImage={element.urlToImage} />
+        <>
+            <InfiniteScroll
+                dataLength={news.length}
+                next = {fetchMoreData}
+                hasMore = {news.length !== totalResults}
+            >
+
+                <div className='news_container'>
+                    <div className="row">
+                        {news.map((element) => (
+                            <div className="col-md-4" key={uuidv4()}>
+                                <News_items author={element.author} content={element.content} description={element.description} publishedAt={element.publishedAt} title={element.title} url={element.url} urlToImage={element.urlToImage} />
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-        </div>
+                </div>
+
+            </InfiniteScroll>
+        </>
     )
 }
 
--- src/components/News_items.jsx
+++ src/components/News_items.jsx
@@ -6,9 +6,8 @@ const News_items = (props) => {
             <div className="card">
                 <img src={props.urlToImage ? props.urlToImage: 'https://www.videogameschronicle.com/files/2022/08/FZtTYwsWYAIFD73.jpg'}  className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <a href={props.url} className='card-title'>{props.title}</a>
+                    <p className="card-text">{props.description}</p>
                 </div>
             </div>
         </div>
